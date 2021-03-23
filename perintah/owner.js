0df1ba3eac4fa6ae540bf8920dce52b255862996
M Ismail NuraAmsa <80862172+INURAMSA@users.noreply.github.com>
2021-03-24 01:13:22 +0700

    Update and rename commands/owner.js to perintah/owner.js

--- perintah/owner.js
+++ perintah/owner.js
@@ -4,8 +4,8 @@ const { text, extendedText, contact, location, liveLocation, image, video, stick
 exports.run = (bot, message, args, from, id) => {
     const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
             + 'VERSION:3.0\n' 
-            + 'FN:ZefianAlfian\n' // full name
-            + 'ORG:ZefianAlfian;\n' // the organization of the contact
+            + 'FN:IsmailTheArt\n' // full name
+            + 'ORG:IsmailTheArt;\n' // the organization of the contact
             + 'TEL;type=CELL;type=VOICE;waid=6289630171792:+62 896 3017 1792\n' // WhatsApp ID + phone number
             + 'END:VCARD'
             bot.sendMessage(from, {displayname: "Zefian", vcard: vcard}, contact, { quoted: message })
